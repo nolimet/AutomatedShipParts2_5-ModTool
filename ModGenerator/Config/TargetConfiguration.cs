160858a7e42c6b6a3f45973da69f9c6c8c4a340b
jesse stam <nolimet@stamdata.nl>
2025-08-12 21:58:55 +0200

    Added support for ignored parts in TargetConfiguration with default handling

--- ModGenerator/Config/TargetConfiguration.cs
+++ ModGenerator/Config/TargetConfiguration.cs
@@ -3,14 +3,13 @@
 namespace ModGenerator.Config;
 
 [method: JsonConstructor]
-public class TargetConfiguration(string? baseGamePath, List<ulong>? mods)
+public class TargetConfiguration(string? baseGamePath, ulong[]? mods = null, Dictionary<ulong, string[]>? ignoredParts = null)
 {
     public readonly string BaseGamePath = baseGamePath ?? string.Empty;
-    public readonly List<ulong> Mods = mods ?? GetDefaultMods();
+    public readonly ulong[] Mods = mods ?? GetDefaultMods();
+    public readonly Dictionary<ulong, string[]> IgnoredParts = ignoredParts ?? GetDefaultIgnoredParts();
 
-    public TargetConfiguration(string baseGamePath) : this(baseGamePath, GetDefaultMods()) { }
-
-    private static List<ulong> GetDefaultMods() =>
+    private static ulong[] GetDefaultMods() =>
     [
         2888343841,
         2886141879,
@@ -18,4 +17,6 @@ public class TargetConfiguration(string? baseGamePath, List<ulong>? mods)
         3121346591,
         3119349707
     ];
+
+    private static Dictionary<ulong, string[]> GetDefaultIgnoredParts() => new();
 }
